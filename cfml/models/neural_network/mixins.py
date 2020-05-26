16ebe348b7f15e19d4dce664d7bfd2480427fd67
heyujian <syameimaru_kurumi@pku.edu.cn>
2020-05-26 23:55:30 +0800

    实现了 FCNNInitializerMixin

--- cfml/models/neural_network/mixins.py
+++ cfml/models/neural_network/mixins.py
@@ -135,10 +135,36 @@ class FCNNMixin(NormalizeMixin, GradientDescentMixin, metaclass=ABCMeta):
         return self._final_affine(net, *self.wb(len(self.hidden_layers)))
 
 
+class FCNNInitializerMixin:
+    def __init__(self, *,
+                 lb: float = 0.,
+                 lr: float = 3e-4,
+                 loss: str = "l1",
+                 epoch: int = 200,
+                 optimizer: str = "sgd",
+                 initializer: str = "uniform",
+                 initializer_config: Dict[str, Any] = None,
+                 hidden_layers: List[Tuple[int, Union[str, None]]] = None):
+        self._lb = lb
+        self._lr = lr
+        self._epoch = epoch
+        self._opt = optimizer
+        # loss
+        self._loss = Loss(loss)
+        # initializer
+        if initializer_config is None:
+            initializer_config = {}
+        self._initializer = Initializer(initializer, **initializer_config)
+        # hidden layers
+        self._hidden_layers = None
+        if hidden_layers is not None:
+            self._hidden_layers = [Layer(*layer) for layer in hidden_layers]
+
+
 class FCNNRegressorMixin(FCNNMixin, metaclass=ABCMeta):
     def predict(self,
                 x: np.ndarray) -> np.ndarray:
         return self.predict_raw(x)
 
 
-__all__ = ["FCNNRegressorMixin"]
+__all__ = ["FCNNInitializerMixin", "FCNNRegressorMixin"]
