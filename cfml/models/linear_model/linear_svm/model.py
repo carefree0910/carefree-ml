2595e27d942430489efae1d475bdb0dac335d9d3
heyujian <syameimaru_kurumi@pku.edu.cn>
2020-05-26 14:50:56 +0800

    修复、优化了 LinearSVM
    1）即使当前 batch 没有误分类的样本，也要对 w 的 norm 进行惩罚
    2）如果没有误分类的样本，可以节省一些运算量

--- cfml/models/linear_model/linear_svm/model.py
+++ cfml/models/linear_model/linear_svm/model.py
@@ -21,30 +21,31 @@ class LinearSVM(ClassifierBase, LinearMixin):
                       x_batch: np.ndarray,
                       y_batch: np.ndarray,
                       batch_indices: np.ndarray) -> Dict[str, Any]:
-        w_norm = np.linalg.norm(self._w)
         predictions = self._predict_normalized(x_batch)
         diff = 1. - y_batch * predictions
         critical_mask = (diff > 0).ravel()
-        if not np.any(critical_mask):
-            loss = 0.
-        else:
-            loss = 0.5 * w_norm + self._lb * diff[critical_mask].mean()
-        return {"loss": loss, "critical_mask": critical_mask}
+        loss = 0.5 * np.linalg.norm(self._w)
+        has_critical = np.any(critical_mask)
+        if has_critical:
+            loss += self._lb * diff[critical_mask].mean()
+        return {"loss": loss, "has_critical": has_critical, "critical_mask": critical_mask}
 
     def gradient_function(self,
                           x_batch: np.ndarray,
                           y_batch: np.ndarray,
                           batch_indices: np.ndarray,
                           loss_dict: Dict[str, Any]) -> Dict[str, np.ndarray]:
-        if loss_dict["loss"] <= 0.:
-            zero = np.zeros(1, np.float32)
-            return {"_w": zero, "_b": zero}
-        critical_mask = loss_dict["critical_mask"]
-        x_critical, y_critical = x_batch[critical_mask], y_batch[critical_mask]
-        delta = -self._lb * y_critical
-        gradient_dict = {"_w": self._w + (x_critical * delta).sum(0).reshape([-1, 1])}
+        has_critical, critical_mask = map(loss_dict.get, ["has_critical", "critical_mask"])
+        if not has_critical:
+            delta = None
+            gradient_dict = {"_w": self._w}
+        else:
+            x_critical, y_critical = x_batch[critical_mask], y_batch[critical_mask]
+            delta = -self._lb * y_critical
+            gradient_dict = {"_w": self._w + (x_critical * delta).sum(0).reshape([-1, 1])}
         if self.fit_intersect:
-            gradient_dict["_b"] = delta.sum(0).reshape([1, 1])
+            gb = np.zeros([1, 1], np.float32) if delta is None else delta.sum(0).reshape([1, 1])
+            gradient_dict["_b"] = gb
         return gradient_dict
 
     def fit(self,
