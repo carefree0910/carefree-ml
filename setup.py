847e001e55d6325b5ca6d827adf9cc67843edce7
heyujian <syameimaru_kurumi@pku.edu.cn>
2020-06-11 21:00:51 +0800

    更新了 carefree-data 的依赖

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1.0",
     packages=find_packages(exclude=("tests",)),
     install_requires=[
-        "carefree-data",
+        "carefree-data>=0.1.2",
         "carefree-toolkit>=0.1.5",
         "dill", "future", "psutil", "pillow",
         "cython>=0.29.12", "numpy>=1.16.2", "scipy>=1.2.1",
