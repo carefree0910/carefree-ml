66cfb45428a9dcc7ac3de0974c4da33fbda846dc
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2020-12-12 16:27:57 +0800

    删除了 `docs` 相关的东西

--- setup.py
+++ setup.py
@@ -16,8 +16,6 @@ setup(
         "dill", "future", "psutil", "pillow",
         "cython>=0.29.12", "numpy>=1.16.2", "scipy>=1.2.1",
         "scikit-learn>=0.20.3", "matplotlib>=3.0.3",
-        "mkdocs", "mkdocs-material", "mkdocs-minify-plugin",
-        "Pygments", "pymdown-extensions"
     ],
     author="carefree0910",
     author_email="syameimaru_kurumi@pku.edu.cn",
